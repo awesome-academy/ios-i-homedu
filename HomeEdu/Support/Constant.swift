c6d9221f5c2eebe84110d631368b18366b38045a
Cuong Nguyen <cmazik@Nguyens-MacBook-Pro.local>
2019-07-30 15:16:56 +0700

    Fix Comment

---
+++ HomeEdu/Support/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  HomeEdu
+//
+//  Created by Nguyen Cuong on 7/31/19.
+//  Copyright © 2019 Nguyen Cuong. All rights reserved.
+//
+
+enum Constant{
+    static let accountId = "Account"
+    static let examScheduleId = "ExamSchedule"
+    static let scheduleId = "Schedule"
+    static let viewScoreId = "ViewScore"
+    static let aboutId = "About"
+}
