36af93b869fbbfd12a85806cada85b957070fa46
Cuong Nguyen <cmazik@Nguyens-MacBook-Pro.local>
2019-07-30 16:03:24 +0700

    Create Tabbar

---
+++ HomeEdu/Scenes/Main/TabBar/TabbarController.swift
@@ -0,0 +1,38 @@
+//
+//  TabbarController.swift
+//  HomeEdu
+//
+//  Created by Nguyen Cuong on 7/30/19.
+//  Copyright © 2019 Nguyen Cuong. All rights reserved.
+//
+
+import UIKit
+
+class TabbarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+
+    func setup(){
+        let taikhoan = Storyboards.Taikhoan
+        let lichthi = Storyboards.Lichthi
+        let tkb = Storyboards.Tkb
+        let xemdiem = Storyboards.Xemdiem
+        let about = Storyboards.About
+        
+        let taikhoan_controller = taikhoan.instantiateViewController(withIdentifier: "taikhoan")
+        let lichthi_controller = lichthi.instantiateViewController(withIdentifier: "lichthi")
+        let tkb_controller = tkb.instantiateViewController(withIdentifier: "tkb")
+        let xemdiem_controller = xemdiem.instantiateViewController(withIdentifier: "xemdiem")
+        let about_controller = about.instantiateViewController(withIdentifier: "about")
+        
+        xemdiem_controller.tabBarItem = UITabBarItem(title: "Xem điểm", image: nil, tag: 0)
+        tkb_controller.tabBarItem = UITabBarItem(title: "TKB", image: nil, tag: 1)
+        taikhoan_controller.tabBarItem = UITabBarItem(title: "Tài khoản", image: nil, tag: 2)
+        lichthi_controller.tabBarItem = UITabBarItem(title: "Lịch thi", image: nil, tag: 3)
+        about_controller.tabBarItem = UITabBarItem(title: "About", image: nil, tag: 4)
+        self.viewControllers = [xemdiem_controller, tkb_controller, taikhoan_controller, lichthi_controller, about_controller]
+    }
+}
