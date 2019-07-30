df3f5a0e9615ca7bdf73163a51dab4d7a7869668
Cuong Nguyen <cmazik@Nguyens-MacBook-Pro.local>
2019-07-30 14:40:27 +0700

    Init project

---
+++ HomeEdu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeEdu
+//
+//  Created by Nguyen Cuong on 7/30/19.
+//  Copyright © 2019 Nguyen Cuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
