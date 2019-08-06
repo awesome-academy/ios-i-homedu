03f72ea2d2b6a11e7d5aeadcb408e564ff7057f9
minmon98 <phivanminh10@hotmail.com>
2019-08-06 09:27:29 +0700

    new exam branch

---
+++ HomeEdu/Scenes/Main/ExamSchedule/ExamScheduleController.swift
@@ -0,0 +1,18 @@
+//
+//  ExamScheduleController.swift
+//  HomeEdu
+//
+//  Created by Minh Mon on 8/6/19.
+//  Copyright © 2019 Nguyen Cuong. All rights reserved.
+//
+
+import UIKit
+
+class ExamScheduleController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
